ca62ee6fa6cc7514ab4d286ba908c57e12cad998
grzeski <grzegorz.rzeski@autonomik.pl>
2020-12-02 20:44:17 +0100

    added test for checker after 17

--- tests/test_Checker.py
+++ tests/test_Checker.py
@@ -16,4 +16,16 @@ class TestChecker(unittest.TestCase):
 
         result = checker.player.wavWasPlayed(fileTest)
         self.assertEqual(result, False)
+    
+    def test_checker_after_17(self):
+        checker = Checker()
+        fileTest = "File.wav"
+        checker.player.getTime = Mock(name = "getTime")
+        checker.player.getTime.return_value = 21
+        
+        checker.player.wavWasPlayed = Mock(name = "wavWasPlayed")
+        checker.player.wavWasPlayed.return_value = True
+
+        result = checker.player.wavWasPlayed(fileTest)
+        self.assertEqual(result, True)
 
