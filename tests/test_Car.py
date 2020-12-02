dae54b51535fcb2f7104871c7a6b9f96eb78da41
grzeski <grzegorz.rzeski@autonomik.pl>
2020-12-02 19:54:10 +0100

    added test for driveTo

--- tests/test_Car.py
+++ tests/test_Car.py
@@ -5,7 +5,7 @@ from src.sample.CarClass import Car
 
 class TestCar(unittest.TestCase):
 
-    def test_needsFuel(self):
+    def test_needs_fuel(self):
         car = Car()
         car.needsFuel = Mock(name = "needsFuel")
         car.needsFuel.return_value = True
@@ -18,6 +18,14 @@ class TestCar(unittest.TestCase):
         car.getEngineTemperature.return_value = 115
 
         self.assertEqual(car.getEngineTemperature(), 115)
+
+    def test_drive_to(self):
+        car = Car()
+        destination = "Gdansk"
+        car.driveTo = Mock(name = "driveTo")
+        car.driveTo.return_value = f"Our destination is {destination}"
+
+        self.assertEqual(car.driveTo("Gdansk"), "Our destination is Gdansk")
 
 
 if __name__ == '__main__':
